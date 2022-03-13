57a04c74aa5bca538cfe217f49bb043e5892492c
WannK-IT <n.nquanght@gmail.com>
2022-03-14 04:06:15 +0700

    init project

---
+++ html/script.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="libs/bootstrap/js/jquery-3.6.0.js"></script>
+<script type="text/javascript" src="libs/bootstrap/js/popper.min.js"></script>
+<script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="libs/bootstrap/js/main.js"></script>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once "html/head.php"?>
+</head>
+
+<body>
+
+    <!-- -- Script -- -->
+    <?php require_once "html/script.php"?>
+</body>
+
+</html>
